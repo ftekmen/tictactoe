96104c204a6039ba67df5eb1057621f32f2c1bd7
fatih.tekmen <fatih.tekmen@intra.local>
2021-05-03 22:37:56 +0300

    first commit, initial files.

---
+++ tictactoe.js
@@ -0,0 +1,27 @@
+const gameBoard = (() => {
+  const fields = [9];
+
+  return { fields };
+})();
+
+const game = (() => {
+
+})();
+
+const displayController = (() => {
+  const render = () => {
+
+  };
+
+  return { render };
+})();
+
+const player = () => {
+  const name = "";
+
+  const getName = () => {
+
+  };
+
+  return { getName };
+};
